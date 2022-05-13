4c7c3005352e7a8ff0cff426a4fb5a60aa62b4fc
Marcos Albuquerque <mpdsa20@gmail.com>
2022-05-13 10:31:09 -0300

    Requisito 2.1

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,10 +1,15 @@
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
-    def __len__(self):
-        pass
+    total_pedidos = 0
 
-    def add_new_order(self, customer, order, day):
-        pass
+    def __init__(self):
+        self.total_pedidos
+
+    def __len__(cls):
+        return cls.total_pedidos
+
+    def add_new_order(self):
+        self.total_pedidos += 1
 
     def get_most_ordered_dish_per_customer(self, customer):
         pass
@@ -20,3 +25,7 @@ class TrackOrders:
 
     def get_least_busy_day(self):
         pass
+
+
+teste = TrackOrders()
+print(len(teste))
