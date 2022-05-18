356ec2c51e7b93d857d1f76f08b4a9c443c9307c
Marcos Albuquerque <mpdsa20@gmail.com>
2022-05-18 12:12:43 -0300

    Requisito 2.3

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,6 +1,14 @@
+from collections import defaultdict
+
+
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
     total_pedidos = 0
+    dias_semana = ('segunda-feira', 'terça-feira', 'quarta-feira',
+                   'quinta-feira', 'sexta-feira', 'sábado', 'domingo')
+
+    cliente_pedidos = []
+    prato_favorito = defaultdict(int)
 
     def __init__(self):
         self.total_pedidos
@@ -8,11 +16,21 @@ class TrackOrders:
     def __len__(cls):
         return cls.total_pedidos
 
-    def add_new_order(self):
+    def add_new_order(self, customer, order, day):
         self.total_pedidos += 1
+        self.cliente_pedidos.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        conta_pratos = []
+
+        for i in self.cliente_pedidos:
+            if i[0] == customer:
+                self.prato_favorito[i[1]] += 1
+
+        for i in self.prato_favorito.items():
+            conta_pratos.append(i)
+
+        return conta_pratos[1][0]
 
     def get_never_ordered_per_customer(self, customer):
         pass
@@ -26,6 +44,4 @@ class TrackOrders:
     def get_least_busy_day(self):
         pass
 
-
-teste = TrackOrders()
-print(len(teste))
+    # print(cliente_pedidos)
